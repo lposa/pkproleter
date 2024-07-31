d1430c9fe2c3535a5224918cdf9f3308ecd31462
Leonard <leonard_posa@epam.com>
2024-07-31 15:09:02 +0200

    fix: fixed build

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,109 +3,119 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
-    'react-native/react-native': true,
+    "react-native/react-native": true,
   },
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-native/all',
-    'plugin:react/recommended',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-native/all",
+    "plugin:react/recommended",
 
-    'plugin:import/typescript',
-    'standard-with-typescript',
-    '@react-native-community',
-    'eslint:recommended',
-    'prettier',
+    "plugin:import/typescript",
+    "standard-with-typescript",
+    "@react-native-community",
+    "eslint:recommended",
+    "prettier",
   ],
   settings: {
-    'import/ignore': ['react-native'],
+    "import/ignore": ["react-native"],
   },
-  parser: '@typescript-eslint/parser',
-  plugins: ['react', 'react-native', '@typescript-eslint', 'prettier'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "react-native", "@typescript-eslint", "prettier"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
-    project: './tsconfig.json',
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
   rules: {
-    'no-undef': 'off',
-    'sort-imports': [
-      'error',
+    "no-undef": "off",
+    "sort-imports": [
+      "error",
       {
         ignoreCase: true,
         ignoreDeclarationSort: true,
       },
     ],
-    'import/order': [
+    "import/order": [
       1,
       {
-        groups: ['external', 'builtin', 'internal', 'sibling', 'parent', 'index'],
-        'newlines-between': 'always',
+        groups: [
+          "external",
+          "builtin",
+          "internal",
+          "sibling",
+          "parent",
+          "index",
+        ],
+        "newlines-between": "always",
         alphabetize: {
-          order: 'asc',
+          order: "asc",
           caseInsensitive: true,
         },
       },
     ],
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
 
-    'spaced-comment': ['error', 'always'],
-    '@typescript-eslint/ban-types': 'error',
-    'react-native/no-inline-styles': 'error',
-    'react-native/no-color-literals': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
-    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 0 }],
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/consistent-type-definitions': 'off',
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/promise-function-async': 'off',
-    '@typescript-eslint/prefer-nullish-coalescing': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/restrict-template-expressions': ['error', { allowAny: true }],
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
+    "spaced-comment": ["error", "always"],
+    "@typescript-eslint/ban-types": "error",
+    "react-native/no-inline-styles": "error",
+    "react-native/no-color-literals": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "no-multiple-empty-lines": ["error", { max: 2, maxEOF: 0 }],
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/promise-function-async": "off",
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/restrict-template-expressions": [
+      "error",
+      { allowAny: true },
+    ],
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
       {
-        prefer: 'type-imports',
+        prefer: "type-imports",
       },
     ],
-    'react/jsx-pascal-case': [
-      'error',
+    "react/jsx-pascal-case": [
+      "error",
       {
         allowAllCaps: true,
         ignore: [],
       },
     ],
-    'react-native/no-raw-text': [
-      'error',
+    "react-native/no-raw-text": [
+      "error",
       {
-        skip: ['TextInput'],
+        skip: ["TextInput"],
       },
     ],
-    '@typescript-eslint/naming-convention': [
-      'error',
+    "@typescript-eslint/naming-convention": [
+      "error",
       {
-        selector: 'variable',
-        types: ['boolean'],
-        format: ['PascalCase', 'camelCase'],
-        prefix: ['is', 'should', 'has', 'can', 'did', 'will'],
+        selector: "variable",
+        types: ["boolean"],
+        format: ["PascalCase", "camelCase"],
+        prefix: ["is", "should", "has", "can", "did", "will"],
       },
       {
-        selector: 'interface',
-        format: ['PascalCase'],
+        selector: "interface",
+        format: ["PascalCase"],
         custom: {
-          regex: '^I[A-Z]',
+          regex: "^I[A-Z]",
           match: true,
         },
       },
     ],
-    '@typescript-eslint/no-misused-promises': [
-      'error',
+    "@typescript-eslint/no-misused-promises": [
+      "error",
       {
         checksVoidReturn: {
           arguments: false,
@@ -113,11 +123,11 @@ module.exports = {
         },
       },
     ],
-    '@typescript-eslint/ban-ts-comment': [
-      'error',
+    "@typescript-eslint/ban-ts-comment": [
+      "error",
       {
-        'ts-ignore': 'allow-with-description',
+        "ts-ignore": "allow-with-description",
       },
     ],
   },
-}
+};
--- babel.config.js
+++ babel.config.js
@@ -2,18 +2,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["./src"], // Adjust if your source files are in a different folder
-          alias: {
-            "@": "./src", // Adjust the path based on your project structure
-            "@components": "./src/components", // Specific alias for components
-          },
-        },
-      ],
-    ],
+    presets: ["babel-preset-expo"],
   };
 };
