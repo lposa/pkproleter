d4ec1930ca1d045bd75907932b5a89c7bfb9b6e2
Leonard <leonard_posa@epam.com>
2024-07-31 15:02:04 +0200

    project setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"], // Adjust if your source files are in a different folder
+          alias: {
+            "@": "./src", // Adjust the path based on your project structure
+            "@components": "./src/components", // Specific alias for components
+          },
+        },
+      ],
+    ],
   };
 };
